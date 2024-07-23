beec84171a44b91648808b19ee8e9a67d6453eaa
devsany <sunnyrajbodhgaya19@gmail.com>
2024-07-23 17:21:39 +0530

    getByLableText()

--- src/Lable.jsx
+++ src/Lable.jsx
@@ -3,10 +3,17 @@ import React from "react";
 const Lable = () => {
   return (
     <div>
-      <label htmlFor="">sunny</label>
-      <label htmlFor="">raj</label>
-      <label htmlFor="">kumar</label>
-      <label htmlFor="">mobile</label>
+      <form action="">
+        <label htmlFor="sunnya">sunny</label>
+        <input type="text" id="sunnya" />
+        <label htmlFor="raj">sunny</label>
+
+        <select name="" id="raj">
+          <option value="">Select the india</option>
+          <option value="">India</option>
+          <option value="">USA</option>
+        </select>
+      </form>
     </div>
   );
 };
--- src/Lable.test.jsx
+++ src/Lable.test.jsx
@@ -1,6 +1,16 @@
-import {render,screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Lable from "./Lable";
 
-describe("Lable test",()=>{
-    
-}
+describe("Lable test", () => {
+  test("label test", () => {
+    render(<Lable />);
+
+    const lable1 = screen.getByLabelText("sunny", {
+      selector: "input",
+    });
+    expect(lable1).toBeInTheDocument();
+
+    // const lable2 = screen.getByLabelText("sunny" )
+    // expect(lable2).toBeInTheDocument();
+  });
+});
